aba89f9efa375d53bbd512854779d8036f7c09fe
Morgan <jmorgansmith88@gmail.com>
2020-06-12 16:56:53 -0400

    cleaned up code and added a call to eyefinity.py

--- unified.py
+++ unified.py
@@ -3,48 +3,54 @@ import time
 import urllib.request
 import secrets
 
-#function to export the image selected from unified
-def downloadImage():
+#function to export the image selected from Unified
+def DownloadImage():
     moreOptions = browser.find_element_by_xpath('/html/body/div[1]/div[2]/div/div/div/div[2]/div[1]/button[8]')
     moreOptions.click()
     clickDownload = browser.find_element_by_xpath('/html/body/div[1]/div[2]/div/div/div/div[2]/div[1]/button[11]/i')
     clickDownload.click()
 
-    currWindow = browser.window_handles[0]
+    currWindow = browser.window_handles[0]#save current browser tab before we switch to new tab
     newTab = browser.window_handles[1]  #switch to new tab containg the .png
     browser.switch_to_window(newTab)
 
     imagePath = browser.find_element_by_xpath('/html/body/img')
     imageSrc = imagePath.get_attribute('src')
-    print(imageSrc) #remove this after testing
-    urllib.request.urlretrieve(imageSrc, 'C:/Users/jmorg/Downloads/test.png')
-    browser.close()
+    urllib.request.urlretrieve(imageSrc, 'C:/Users/jmorg/Downloads/test.png')   #save image to drive
+
+    browser.close() #close newTab after we collect the image. Switch back to original tab.
     browser.switch_to_window(currWindow)
+#end DownloadImage
 
-#unified needs to unselect the current photo to select the next. 
-# To download another photo, reset must be called 
+#Unified needs to unselect the current photo to select the next. 
+#To download another photo, reset must be called 
 def reset():
-        #clicks the first square to bringphoto thumbnails back
-        square = browser.find_element_by_xpath('/html/body/div[3]/div/div/div/ng-transclude/button/i').click() 
-        unselect = browser.find_element_by_xpath('/html/body/div[3]/div/div/div/ng-transclude/div[1]/div[1]/a[1]/div[2]').click()
+    #clicks the first square to bringphoto thumbnails back
+    square = browser.find_element_by_xpath('/html/body/div[3]/div/div/div/ng-transclude/button/i').click() 
+    unselect = browser.find_element_by_xpath('/html/body/div[3]/div/div/div/ng-transclude/div[1]/div[1]/a[1]/div[2]').click()
+#end reset
 
-
+#webdriver to be used for exporting images from Unified
 browser = webdriver.Chrome('C:/Users//jmorg/Downloads/chromedriver_win32/chromedriver')
 browser.get('http://unified.gallery')
-windowHandler = browser.window_handles
-
 time.sleep(6)
-#Variables for the username,password, and login elements
-userField = browser.find_element_by_xpath('//*[@id="login-container"]/div/div[2]/div/div/form/div[1]/input')  
-passField = browser.find_element_by_xpath('//*[@id="login-container"]/div/div[2]/div/div/form/div[2]/input') 
-loginButton = browser.find_element_by_class_name('login-button.ng-scope')
 
-#enter username and password
-userField.send_keys(secrets.uniUser)
-passField.send_keys(secrets.uniPass)
-loginButton.click()
+#Login to Unified.gallery
+def UniLogin():
+    #Variables for the username,password, and login elements
+    userField = browser.find_element_by_xpath('//*[@id="login-container"]/div/div[2]/div/div/form/div[1]/input')  
+    passField = browser.find_element_by_xpath('//*[@id="login-container"]/div/div[2]/div/div/form/div[2]/input') 
+    loginButton = browser.find_element_by_class_name('login-button.ng-scope')
 
-time.sleep(6)
+    #enter username and password
+    userField.send_keys(secrets.uniUser)
+    passField.send_keys(secrets.uniPass)
+    loginButton.click()
+    time.sleep(4)
+#end UniLogin
+
+UniLogin()
+
 #Acessing search bar once logged in
 searchBar = browser.find_element_by_id('search-input')
 searchBar.send_keys(secrets.patient)
@@ -59,9 +65,14 @@ time.sleep(3)
 firstSelect = browser.find_element_by_xpath('/html/body/div[3]/div/div/div/ng-transclude/div[1]/div[1]/a[1]/img')
 firstSelect.click()
 time.sleep(4)
-downloadImage()
+
+DownloadImage()
 reset()
+time.sleep(2)
+#visual fields and gluacoma suspects will have 2 images per patient.
+#Mac Degen patients will only have one. Find a way to select images by dates?
 
-
+browser.close()
+import eyefinity
 
 
